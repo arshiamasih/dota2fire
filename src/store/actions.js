4625e17c35007b048325867b994f57bfc471b3e5
arshiamasih <arshia.masih@gmail.com>
2020-01-26 12:38:50 -0800

    adds simple dummy redux implementation into the app

---
+++ src/store/actions.js
@@ -0,0 +1,6 @@
+import ADD_WINNERS from './types'
+
+export const addWinners = () => ({
+  type: ADD_WINNERS
+})
+
---
+++ src/store/types.js
@@ -0,0 +1,8 @@
+//add winners to next round (round++)
+export const ADD_WINNERS = 'ADD_WINNERS';
+
+//api error message handling
+export const CALL_API = 'CALL_API';
+export const RECEIVE_API = 'RECEIVE_API';
+export const API_FAILURE = 'API_FAILURE';
+export const CLEAR_API = 'CLEAR_API';
